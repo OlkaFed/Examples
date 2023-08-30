cf47903018a5594b08a2256f3fefafdecb5c37d9
Olya <Olya.fedorowskaya@yandex.ru>
2023-08-31 00:19:20 +0300

    add Example006_IfElse project

---
+++ Example006_IfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "олька")
+{
+Console.WriteLine("Ура, это же Олька!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
