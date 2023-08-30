6a905c1d004ebe77be37a9091cf47f1a0fde1563
Olya <Olya.fedorowskaya@yandex.ru>
2023-08-30 23:56:27 +0300

    add Example005_Random project

---
+++ Example005_Random/Program.cs
@@ -0,0 +1,6 @@
+﻿int numberA = new Random().Next(1, 10); //Случайное целое число в диапазоне от 1 до 9, то есть 1,2,3,4,5,6,7,8,9. 
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
