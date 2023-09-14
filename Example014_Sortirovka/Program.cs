927bfaf31b1415be1b19cb55c898ab27b857ea65
Olya <Olya.fedorowskaya@yandex.ru>
2023-09-14 21:07:18 +0300

    Example014_Sortirovka

---
+++ Example014_Sortirovka/Program.cs
@@ -0,0 +1,34 @@
+﻿// Сортировка 
+
+int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+
+//Опишем метод, который выводит на экран наш массив 
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for(int i=0; i<count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+//Опишем метод, который будет упорядочивать наш массив 
+void SelectionSort(int[] array) 
+{
+    for(int i=0; i<array.Length-1; i++)
+    {
+        int MinPosition = i; 
+        for(int j=i+1; j<array.Length; j++)
+        {if(array[j]<array[MinPosition]) //Для сортировки от большего к меньшему нужно изменить знак{if(array[j]>array[MinPosition])}
+        {MinPosition = j;}}
+
+        int temporary = array[i];
+        array[i] = array[MinPosition];
+        array[MinPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
